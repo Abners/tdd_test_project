056037175735f5899c274a0bdf8c2f338bd7d192
baoxing.peng <peng731647424@126.com>
2015-08-03 15:56:00 +0800

    模板解析类

---
+++ src/com/TemplateParse.java
@@ -0,0 +1,12 @@
+package com;
+
+import java.util.Collections;
+import java.util.List;
+
+public class TemplateParse {
+
+	public List<String> parse(String template) {
+		return Collections.singletonList(template);
+	}
+
+}
--- test/tdd_test_project/RegexLearningTest.java
+++ test/tdd_test_project/RegexLearningTest.java
@@ -21,6 +21,23 @@ public class RegexLearningTest {
 		String stack = "needles of needles nad";
 		String regex = "(needle)";
 		Matcher matcher = Pattern.compile(regex).matcher(stack);
-		assertEquals(2,matcher.groupCount());
+		//matcher.groupCount()方法返回的是正则表达式本身包含的子表达式
+		assertEquals(1,matcher.groupCount());
+	}
+	
+	@Test
+	public void testFindStartAndEnd(){
+		String stack = "the needle shop sells needles";
+		String regex = "(needle)";
+		Matcher matcher = Pattern.compile(regex).matcher(stack);
+		assertTrue(matcher.find());
+		assertEquals("Wrong start index of 1st match",4, matcher.start());
+		assertEquals("Wrong end index of 1st math, actual",10,matcher.end());
+		
+		assertTrue(matcher.find());
+		assertEquals("Wrong start index of 1st match",22, matcher.start());
+		assertEquals("Wrong end index of 1st math, actual",28,matcher.end());
+		
+		assertFalse("should not have any more matches",matcher.find());
 	}
 }
